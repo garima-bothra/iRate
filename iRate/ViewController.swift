9650bcaa0d020e620fb4abb601194240d398f604
garima94921 <gaarimabothra@gmail.com>
2020-06-16 16:48:09 +0530

    Initial Commit

---
+++ iRate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iRate
+//
+//  Created by Garima Bothra on 16/06/20.
+//  Copyright © 2020 Garima Bothra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
