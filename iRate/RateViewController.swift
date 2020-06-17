e5d14c3d04d37d7b46541040bfec790a2effcdb5
garima94921 <gaarimabothra@gmail.com>
2020-06-17 20:17:55 +0530

    Homescreen ready

--- iRate/RateViewController.swift
+++ iRate/RateViewController.swift
@@ -10,21 +10,15 @@ import UIKit
 
 class RateViewController: UIViewController {
 
+    @IBOutlet weak var rating: UISlider!
+    var rateLimit: Int!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func saveButtonPressed(_ sender: Any) {
+        print("Rate: \(rating.value)")
     }
-    */
 
 }
--- iRate/ViewController.swift
+++ iRate/ViewController.swift
@@ -10,11 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ratingStepper: UIStepper!
+    @IBOutlet weak var rateLabel: UILabel!
+    @IBOutlet weak var rateButton: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        ratingStepper.maximumValue = 9.0
+        ratingStepper.minimumValue = 2.0
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        rateLabel.text = Int(sender.value).description
+        rateButton.setTitle("Rating 1-\(Int(sender.value))", for: UIControl.State.normal) 
+    }
+
+    @IBAction func rateButtonPressed(_ sender: Any) {
+        performSegue(withIdentifier: "rating", sender: Any.self)
+    }
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "rating" {
+            let destination = segue.destination as! RateViewController
+            destination.rateLimit = Int(ratingStepper.value)
+        }
+    }
 
 }
 
