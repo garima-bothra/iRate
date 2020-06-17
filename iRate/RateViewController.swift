abd73ead9463ce3fc2c263c5f9e5391a7c461e6b
garima94921 <gaarimabothra@gmail.com>
2020-06-17 19:37:39 +0530

    Feat.: Created rate view

---
+++ iRate/RateViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RateViewController.swift
+//  iRate
+//
+//  Created by Garima Bothra on 17/06/20.
+//  Copyright © 2020 Garima Bothra. All rights reserved.
+//
+
+import UIKit
+
+class RateViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
